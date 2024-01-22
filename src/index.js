845b4962915293fca9e9acc3da6599c62128229d
sarainbloom <152415071+sarainbloom@users.noreply.github.com>
2024-01-21 19:07:10 -0500

    fixed issue with time (minutes)

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ function formatDate(date) {
   let day = days[date.getDay()];
 
   if (minutes < 10) {
-    minutes = `0 ${minutes}`;
+    minutes = `0${minutes}`;
   }
 
   return `${day} ${hours}:${minutes}`;
